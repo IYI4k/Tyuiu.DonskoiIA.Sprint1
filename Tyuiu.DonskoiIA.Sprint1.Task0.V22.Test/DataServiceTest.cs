f9ded40ec55b2965091ffffa4a933dbc836e70be
IYI4k <IYI4k@yandex.ru>
2024-09-18 17:43:08 +0500

    Add project files.

---
+++ Tyuiu.DonskoiIA.Sprint1.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DonskoiIA.Sprint1.Task0.V22.Lib;
+
+namespace Tyuiu.DonskoiIA.Sprint1.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
