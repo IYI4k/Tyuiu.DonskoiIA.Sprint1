8f98f1694b623c9f4cf52d44fe9dfecba976b5e1
IYI4k <IYI4k@yandex.ru>
2024-09-20 10:22:51 +0500

    Task2

---
+++ Tyuiu.DonskoiIA.Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DonskoiIA.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.DonskoiIA.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.ConvertKmToM(x);
+            Assert.AreEqual(1.609, res);
+        }
+    }
+}
---
+++ Tyuiu.DonskoiIA.Sprint1.Task2.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DonskoiIA.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.DonskoiIA.Sprint1.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Донской И. А. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Донской Иван Андреевич | АСОиУб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* переводит введённое количество километров в мили по формуле x * 1,609 и *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ConvertKmToM(x));
+
+            Console.ReadLine();
+        }
+    }
+}
